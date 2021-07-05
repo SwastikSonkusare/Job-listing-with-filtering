4286be26715fe039daf022d22dfec0cbc11d7759
Swastik <swastik.sonkusare18@gmail.com>
2021-07-05 18:11:52 +0530

    added: card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,49 @@
-import React, { useState, useEffect } from "react";
-
+import React from "react";
 
 import data from "../../data.json";
-// import logo from '../../assets/images/account.svg'
 
 import "./Card.scss";
 const Card = () => {
-
   return (
     <div className="card">
       {data.map((item) => (
         <div className="card__container">
-          <img className="card__logo" src={item.logo} alt={item.logo}></img>
-         
+          <div className="card__info">
+            <img className="card__logo" src={item.logo} alt={item.logo}></img>
+
+            <div className="card__left-section">
+              <div className="card__details">
+                <small className="card__company-name">{item.company}</small>
+                <div className="card__job">
+                  <span className={item.new ? "card__job--new active" : "card__job--new"}>
+                    {item.new ? "new!" : ""}
+                  </span>
+                  <span className={item.featured ? "card__job--featured active" : "card__job--featured"}>
+                    {item.featured ? "featured" : ""}
+                  </span>
+                </div>
+              </div>
+
+              <h1 className="card__position">{item.position}</h1>
+
+              <div className="card__content">
+                <small className="card__posted-at">{item.postedAt}</small>
+                <small className="card__contract">{item.contract}</small>
+                <small className="card__location">{item.location}</small>
+              </div>
+            </div>
+          </div>
+
+          <div className="card__categories">
+            {item.languages.map((language) => (
+              <span className="card__category">{language}</span>
+            ))}
+            {item.tools.map((tool) => (
+              <span className="card__category">{tool}</span>
+            ))}
+            <span className="card__category">{item.role}</span>
+            <span className="card__category">{item.level}</span>
+          </div>
         </div>
       ))}
     </div>
